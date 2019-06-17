43ccd16199ef2b86c2a25ea4dc702a2fa580fa83
Jun_T <taka.jun.nelu@icloud.com>
2019-06-17 10:02:02 +0900

    プレイヤーの移動、射撃、死亡、残機管理

---
+++ Snowman/Assets/Takahashi/Scripts/PlayerStatus.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatus : MonoBehaviour
+{
+    [SerializeField,Header("プレイヤーの残機")]
+    public int Hp;
+    [SerializeField, Header("プレイヤーの弾")]
+    public GameObject Bullet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        #region 移動処理
+
+        float x = Input.GetAxis("Horizontal") / 2;
+        float z = Input.GetAxis("Vertical") / 2;
+
+        transform.position += new Vector3(x, 0, z);
+
+        #endregion
+
+        #region 残機管理
+
+        if (Hp==10)
+        {
+            Destroy(gameObject);
+        }
+
+        #endregion
+
+        #region 射撃管理
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Instantiate(Bullet,this.transform.position,Quaternion.identity);
+        }
+        #endregion
+    }
+}
