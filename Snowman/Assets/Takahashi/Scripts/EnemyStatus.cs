2bc4ffebc6a2765cc4b3451b6a15c24258ecf799
Jun_T <taka.jun.nelu@icloud.com>
2019-06-21 13:25:56 +0900

    Enemy生成と行動設定

---
+++ Snowman/Assets/Takahashi/Scripts/EnemyStatus.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStatus : MonoBehaviour
+{
+    private List<Vector3> moveP;
+    public int Speed;
+    
+    private Vector3 target;
+    private int targetC;
+    private Vector3 LastPos;
+
+    // Start is called before the first frame update
+    public void SetPosition(List<Vector3> moveP)
+    {
+        this.moveP = moveP;
+        targetC = 1;
+        target = moveP[targetC];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position == moveP[targetC])
+        {
+            targetC++;
+            target = moveP[targetC];
+        }
+        
+        transform.position = Vector3.Lerp(transform.position, target, Time.deltaTime* Speed);
+
+        if (transform.position == moveP[moveP.Count -1])
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.tag == "BulletP")
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
