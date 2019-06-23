98647667ae46e288edfa85e632e98c9191113634
酒井 悠 <sakai-yuu-1203@docomo.ne.jp>
2019-06-23 17:11:12 +0900

    add.テスラコイルの画像 fix.テスラコイルの放電攻撃をボスに追加

---
+++ Snowman/Assets/Sakai/Scripts/CoilMove.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoilMove : MonoBehaviour
+{
+    GameObject Coil = null;
+    Vector3 distance;
+    float num;
+
+    public bool SparkFlag;
+
+    bool MoveFlag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SparkFlag = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MoveFlag)
+        {
+            transform.position = Vector3.Lerp(transform.position, distance, num);
+        }
+        if (Vector3.Distance(transform.position, distance) < 0.5f && !SparkFlag)
+        {
+            SparkFlag = true;
+        }
+        
+    }
+
+    public void Move(Vector3 distance, float num)
+    {
+        MoveFlag = true;
+        this.distance = distance;
+        this.num = num;
+    }
+
+    public void Spark(int size)
+    {
+        Debug.Log(555);
+        transform.localScale = new Vector3(size, 0.1f, size);
+        SparkFlag = false;
+    }
+}
--- Snowman/Assets/Scripts/BossScripts/BossManager.cs
+++ Snowman/Assets/Scripts/BossScripts/BossManager.cs
@@ -5,23 +5,33 @@ using UnityEngine;
 
 public class BossManager : MonoBehaviour
 {
-    private CharaAttribute BossAttribute = CharaAttribute.minus;
-
     private int BossHp;
     [SerializeField]
     private BossMove bossMove;
-    
+    [SerializeField]
+    private SparkLiner sparkLiner;
+
+    public bool sparkFlag = false;
+
     // Start is called before the first frame update
     void Start()
     {
         BossHp = 10;
-        
-        
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            transform.gameObject.SetActive(false);
+        }
+        if(Input.GetKeyDown(KeyCode.H))
+        {
+            SparkLineFlag();
+        }
 
     }
 
@@ -29,5 +39,10 @@ public class BossManager : MonoBehaviour
     {
         BossHp -= damage;
         return BossHp;
+    }
+
+    public bool SparkLineFlag()
+    {
+        return sparkFlag = true;
     }
 }
