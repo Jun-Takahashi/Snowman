6a23e0dcd715d3c8d24e6f41d92796c1b8afddec
酒井 悠 <sakai-yuu-1203@docomo.ne.jp>
2019-06-25 22:31:12 +0900

    fix.Scriptの整理とファイルの配置の直し ボスの体力が一定以下でコイルを飛ばすように変更

---
+++ Snowman/Assets/Sakai/Scripts/BossScripts/CoilMove.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoilMove : MonoBehaviour
+{
+    Vector3 distance; //目的地（ローカル）
+    float num; //速度（ローカル）
+
+    public bool SparkFlag; //放電してるかしてないか
+
+    private bool MoveFlag; //動かすフラグ
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SparkFlag = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MoveFlag)
+        {
+            GoToPoint(transform.root.gameObject, distance, num);
+        }
+        if (DistanceObject(transform.root.gameObject, distance) && !SparkFlag)
+        {
+            SparkFlag = true;
+        }
+
+    }
+
+    /// <summary>
+    /// 何処へ向かわせるか
+    /// </summary>
+    /// <param name="distance">目的地</param>
+    /// <param name="num">速度</param>
+    public void Move(Vector3 distance, float num)
+    {
+        MoveFlag = true;
+        this.distance = distance;
+        this.num = num;
+    }
+
+    /// <summary>
+    /// 放電させる
+    /// </summary>
+    /// <param name="size">放電のサイズ</param>
+    public void Spark(int size)
+    {
+        transform.localScale = new Vector3(size, 0.1f, size);
+        SparkFlag = false;
+    }
+
+    /// <summary>
+    /// その場所へ向かわせる
+    /// </summary>
+    /// <param name="obj">向かわせるオブジェクト</param>
+    /// <param name="point">向かわせる場所</param>
+    /// <param name="number">向かわせる速度</param>
+    void GoToPoint(GameObject obj, Vector3 point, float number)
+    {
+        obj.transform.position = Vector3.Lerp(transform.position, point, number);
+    }
+
+    /// <summary>
+    /// オブジェクトの距離を測る（目的地に着いているか）
+    /// </summary>
+    /// <param name="obj">オブジェクト</param>
+    /// <param name="point">目的の場所</param>
+    /// <returns>距離が一致していたらtrueを返す</returns>
+    bool DistanceObject(GameObject obj, Vector3 point)
+    {
+        bool flag = Vector3.Distance(obj.transform.position, point) < 0.5f;
+        return flag;
+    }
+}
