4669c4eddb0159111a71d8dd9c4e2eedb506d76a
大石　隼 <>
2019-07-02 15:42:35 +0900

    重力っぽいエフェクトや微調整

---
+++ Snowman/Assets/ooishi/script/Rotat.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotat : MonoBehaviour
+{
+    public GameObject gameObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(new Vector3(0, 3, 0));
+    }
+}
