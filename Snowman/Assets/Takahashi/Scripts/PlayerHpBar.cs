a47cf8ddc18aad9c5da3f27277a5c6358bf7ec06
Jun_T <taka.jun.nelu@icloud.com>
2019-07-02 11:03:51 +0900

    ありがとうございます。Hpゲージ(仮)でございます。

---
+++ Snowman/Assets/Takahashi/Scripts/PlayerHpBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHpBar : MonoBehaviour
+{
+    public GameObject Player;
+    [SerializeField,Header("PlayerのHPバー")]
+    public Slider slider;
+
+    private PlayerStatus script;
+    
+    void Start()
+    {
+        script = Player.GetComponent<PlayerStatus>();
+        slider.maxValue = script.Hp;
+    }
+    
+    void Update()
+    {
+        slider.value = script.Hp;
+    }
+}
