5e6c5111bf35b958deed37d7e68828a640843a34
酒井 悠 <sakai-yuu-1203@docomo.ne.jp>
2019-07-12 14:42:16 +0900

    add.パーティクルをカッコよく タイマーを追加

---
+++ Snowman/Assets/Sakai/Scripts/TimeCount.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TimeCount : MonoBehaviour
+{
+    //[SerializeField, Header("現在時間タイマー")]
+    //private float countTime = 0;
+
+    [SerializeField, Header("制限時間")]
+    private float countDown = 180;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //countTime += Time.deltaTime;
+        if (countDown > 0)
+        {
+            countDown = countDown -= Time.deltaTime;
+            GetComponent<Text>().text = countDown.ToString("F2");
+        }
+        //GetComponent<Text>().text = countTime.ToString("F2");
+        //if (Input.GetKeyDown(KeyCode.O))
+        //{
+        //    SceneManager.LoadScene("sakai");
+        //}
+    }
+}
